c58837b51053d4f379c2eb5047fd8c7712d0cb95
Victor Sebastian Mølschau Lauridsen <vict4694@edu.eal.dk>
2018-09-24 19:03:11 +0200

    ændring i main på pc2

--- git1/Program.cs
+++ git1/Program.cs
@@ -10,7 +10,7 @@ namespace git1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("helo giet1");
+            Console.WriteLine("helo giet1 ændret på pc2");
             Console.WriteLine("new change");
             Console.ReadLine();
         }
